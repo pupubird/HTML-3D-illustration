5929676f5e3d5f57de386b9d672d3841d6c25c14
pupubird <rainchai4240@gmail.com>
2020-07-16 06:49:44 +0800

    uploaded build

---
+++ build/precache-manifest.b674124707a7c307a44e55cfc7dda002.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "76930281447e0ea06be9",
+    "url": "/bundle.9c1ed352.css"
+  },
+  {
+    "revision": "76930281447e0ea06be9",
+    "url": "/bundle.76930281.js"
+  },
+  {
+    "revision": "2fa0fa09f00efd14f95a249fb6570e61",
+    "url": "/assets\\data.json"
+  },
+  {
+    "revision": "e2151fa07db2c9436a8c37cfed96f165",
+    "url": "/assets\\favicon.png"
+  },
+  {
+    "revision": "0b8002302181bec608e0e5a4cffea9c2",
+    "url": "/assets\\icon.png"
+  },
+  {
+    "revision": "beb79c9d093b7a1814ce6fd07cb445c4",
+    "url": "/assets\\manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/assets\\robots.txt"
+  },
+  {
+    "revision": "cb5c085ef2e642a8bdc65b2c47745f7e",
+    "url": "/index.html"
+  }
+];
